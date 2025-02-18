05eeca7af8b33a18d85579efc723149e49638351
yuhangchen <waterkingest@gmail.com>
2025-02-18 18:16:12 -0500

    feat(train): add parallel training support

--- train_pipeline.sh
+++ train_pipeline.sh
@@ -75,7 +75,7 @@ set DTU_TRAINING = "/nfs/speed-scratch/ch_yuhan/dtuTrainingData/dtu/"
 
 echo "Running Training processing..."
 echo "================================================"
-python train_dtu.py --batch_size 1 --epochs 8 --trainpath $DTU_TRAINING --trainlist lists/dtu/train.txt --vallist lists/dtu/val.txt --logdir ./checkpoints $argv
+python train_dtu.py --batch_size 4 --epochs 8 --trainpath $DTU_TRAINING --trainlist lists/dtu/train.txt --vallist lists/dtu/val.txt --logdir ./checkpoints --parallel
 
 conda deactivate
 exit
